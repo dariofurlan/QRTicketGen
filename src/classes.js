16ceed750502caaaf4913b1ff47b37b0f52b7363
Dario Furlan <dario.furlan.51@gmail.com>
2019-09-26 02:03:52 +0200

    updates

--- src/classes.js
+++ src/classes.js
@@ -13,13 +13,14 @@ class Face {
         this.el.style.zIndex = "0";
 
         this.marker = document.createElement('div');
-        this.marker.style.display = "inline-block";
+        this.marker.style.display = "none";
         this.marker.style.position = "absolute";
         this.marker.style.zIndex = "1";
         this.marker.style.top = "50%";
         this.marker.style.left = "50%";
         this.marker.style.transform = "translate(-50%,-50%)";
         this.marker.style.border = "1px dashed white";
+        this.pw = false;
 
         // this.marker.style.filter = "invert(1)";
         this.el.appendChild(this.marker);
@@ -27,10 +28,15 @@ class Face {
     }
 
     refresh() {
-        this.el.style.width = this.width + "mm";
-        this.el.style.height = this.height + "mm";
-        this.marker.style.width = this.width - this.bleed + "mm";
-        this.marker.style.height = this.height - this.bleed + "mm";
+        if (this.pw) {
+            this.marker.style.display = "inline-block";
+        } else {
+            this.marker.style.display = "none";
+        }
+        this.el.style.width = (this.width + 2 * this.bleed) + "mm"; // issues with the parenthesis number tunerd into string
+        this.el.style.height = (this.height + 2 * this.bleed) + "mm";
+        this.marker.style.width = this.width + "mm";
+        this.marker.style.height = this.height + "mm";
         this.el.style.backgroundColor = this.backgroundColor;
         this.el.style.padding = this.bleed;
     }
@@ -96,22 +102,54 @@ class Ticket {
         this.height = 0;
         this.bleed = 0;
         this.qr_size = 0;
+        this.pw = false;
         this.front = new Front();
         this.back = new Back();
         this.refresh();
     }
 
     refresh() {
-        this.front.width = this.width + this.bleed;
-        this.front.height = this.height + this.bleed;
+        this.front.width = this.width;
+        this.front.height = this.height;
         this.front.bleed = this.bleed;
-        this.back.width = this.width + this.bleed;
-        this.back.height = this.height + this.bleed;
-        this.back.bleed = this.bleed;
-        this.back.qr_size = this.qr_size;
+        this.front.pw = this.pw;
         this.front.refresh();
+
+        this.back.width = this.width;
+        this.back.height = this.height;
+        this.back.bleed = this.bleed;
+        this.back.pw = this.pw;
+        this.back.qr_size = this.qr_size;
         this.back.refresh();
     }
+}
+
+function horiz_marker_box() {
+    let marker_box = document.createElement('div');
+    marker_box.style.boxSizing = "content-box";
+    marker_box.style.width = "100%";
+    marker_box.style.height = "100%";
+    marker_box.style.borderLeft = "1px solid black";
+    marker_box.style.borderRight = "1px solid black";
+    return marker_box;
+}
+
+function vert_marker_box() {
+    let marker_box = document.createElement('div');
+    marker_box.style.boxSizing = "content-box";
+    marker_box.style.width = "100%";
+    marker_box.style.height = "100%";
+    marker_box.style.borderTop = "1px solid black";
+    marker_box.style.borderBottom = "1px solid black";
+    return marker_box;
+}
+
+function th() {
+    let el = document.createElement('th');
+    el.style.margin = "0";
+    el.style.padding = "0";
+
+    return el;
 }
 
 function tr() {
@@ -135,8 +173,107 @@ function td(id) {
 
 function appenChilds(parent, child_list) {
     for (let child in child_list) {
-        child_list[child].width=100/child_list.length;
+        child_list[child].width = 100 / child_list.length;
         parent.appendChild(child_list[child]);
+    }
+}
+
+class Grid {
+    constructor() {
+        this.width = 0;
+        this.height = 0;
+        this.rows = 0;
+        this.cols = 0;
+        this.tickets_td = [];
+        this.ticket_width = 0;
+        this.ticket_height = 0;
+        this.marker = 6;
+        this.bleed = 0;
+
+        this.el = document.createElement('div');
+        this.el.style.position = "relative";
+
+        this.nested = document.createElement('div');
+        this.nested.style.position = "absolute";
+        this.nested.style.top = "50%";
+        this.nested.style.left = "50%";
+        this.nested.style.transform = "translate(-50%,-50%)";
+
+        this.el.appendChild(this.nested);
+    }
+
+    refresh() {
+        this.el.style.width = this.width;
+        this.el.style.height = this.height;
+        this.nested.style.width = ((this.ticket_width * this.cols) + 2 * this.marker) + "mm";
+        this.nested.style.height = ((this.ticket_height * this.rows) + 2 * this.marker) + "mm";
+
+        if (this.rows === 0 || this.cols === 0)
+            return;
+        if (this.table !== undefined)
+            this.table.parentNode.removeChild(this.table);
+
+
+        this.table = document.createElement('table');
+        this.table.style.tableLayout = "fixed";
+        this.table.style.width = "100%";
+        this.table.style.height = "100%";
+
+        let pre = tr();
+        for (let r = 0; r < this.cols + 2; r++) {
+            let m = th();
+            if (r === 0 || r === this.cols + 1) {
+                m.style.width = this.marker + "mm";
+            } else {
+                m.style.padding = "0 " + this.bleed + "mm";
+                m.appendChild(horiz_marker_box());
+            }
+            m.style.height = this.marker + "mm";
+            pre.appendChild(m);
+        }
+        this.table.appendChild(pre);
+
+        for (let r = 0; r < this.rows; r++) {
+            let row = tr();
+            row.style.height = this.ticket_height + "mm";
+
+            let m_l = td();
+            m_l.style.height = "100%";
+            m_l.style.padding = this.bleed + "mm 0mm";
+            m_l.appendChild(vert_marker_box());
+            row.appendChild(m_l);
+            for (let c = 0; c < this.cols; c++) {
+                let pos = r * this.cols + c;
+                let col = td("grid" + pos);
+                // col.style.outline = "1px solid black"; //todo developing purpose
+                col.style.width = this.ticket_width;
+                col.style.height = this.ticket_height;
+                this.tickets_td.push(col);
+                row.appendChild(col);
+                console.log("r: " + r + " , c: " + c);
+            }
+            let m_r = td();
+            m_r.style.height = "100%";
+            m_r.style.padding = this.bleed + "mm 0mm";
+            m_r.appendChild(vert_marker_box());
+            row.appendChild(m_r);
+
+            this.table.appendChild(row);
+        }
+        let post = tr();
+        for (let r = 0; r < this.cols + 2; r++) {
+            let m = th();
+            if (r === 0 || r === this.cols + 1) {
+                m.style.width = this.marker + "mm";
+            } else {
+                m.style.padding = "0 " + this.bleed + "mm";
+                m.appendChild(horiz_marker_box());
+            }
+            m.style.height = this.marker + "mm";
+            post.appendChild(m);
+        }
+        this.table.appendChild(post);
+        this.nested.appendChild(this.table);
     }
 }
 
@@ -145,69 +282,79 @@ class Page {
         this.width = 0;
         this.height = 0;
         this.padding = 0;
+        this.pw = false;
+        this.rows = 0;
+        this.cols = 0;
+        this.grid = new Grid();
 
         this.el = document.createElement('div');
-        this.el.style.border = "1px solid black";
+        this.el.style.outline = "1px solid black";
 
         this.table = document.createElement('table');
-        let top = tr();
+        this.tr_top = tr();
         this.top_left_corner = td();
         this.top_left = td('top-left');
         this.top_centre = td('top-centre');
         this.top_right = td('top-right');
         this.top_right_corner = td();
-        appenChilds(top, [this.top_left_corner, this.top_left, this.top_centre, this.top_right, this.top_right_corner]);
-        this.table.appendChild(top);
-        let mid1 = tr();
-        this.mid_left_top = td();
+        appenChilds(this.tr_top, [this.top_left_corner, this.top_left, this.top_centre, this.top_right, this.top_right_corner]);
+        this.table.appendChild(this.tr_top);
+
+        this.tr_mid = tr();
+        this.mid_left = td();
         this.mid_centre = td('content-box');
-        this.mid_centre.rowSpan = "3";
         this.mid_centre.colSpan = "3";
-        this.mid_right_top = td();
-        appenChilds(mid1, [this.mid_left_top, this.mid_centre, this.mid_right_top])
-        let mid2 = tr();
-        this.mid_left_mid = td();
-        this.mid_right_mid = td();
-        appenChilds(mid2, [this.mid_left_mid, this.mid_right_mid]);
-        let mid3 = tr();
-        this.mid_left_bot = td();
-        this.mid_right_bot = td();
-        appenChilds(mid3, [this.mid_left_bot, this.mid_right_bot]);
-        appenChilds(this.table, [mid1,mid2,mid3]);
-        let bot = tr();
+        this.mid_right = td();
+        appenChilds(this.tr_mid, [this.mid_left, this.mid_centre, this.mid_right]);
+        this.table.appendChild(this.tr_mid);
+
+        this.tr_bot = tr();
         this.bot_left_corner = td();
         this.bot_left = td('bot-left');
         this.bot_centre = td('bot-centre');
         this.bot_right = td('bot-right');
         this.bot_right_corner = td();
-        appenChilds(bot, [this.bot_left_corner, this.bot_left, this.bot_centre, this.bot_right, this.bot_right_corner]);
-        this.table.appendChild(bot);
+        appenChilds(this.tr_bot, [this.bot_left_corner, this.bot_left, this.bot_centre, this.bot_right, this.bot_right_corner]);
+        this.table.appendChild(this.tr_bot);
         // this.table.appendChild(this.bot);
 
         this.el.appendChild(this.table);
+        this.mid_centre.appendChild(this.grid.el);
     }
 
     refresh() {
-        let left = [this.top_left_corner, this.mid_left_top, this.mid_left_mid, this.mid_left_bot, this.bot_left_corner];
-        for (let el in left) {
-            left[el].style.width = this.padding+"mm";
+        let left_right = [this.top_left_corner, this.mid_left, this.bot_left_corner, this.top_right_corner, this.mid_right, this.bot_right_corner];
+        for (let el in left_right) {
+            left_right[el].style.width = this.padding + "mm";
+            left_right[el].style.outline = (this.pw) ? "1px dotted black" : "";
         }
-        let right = [this.top_right_corner, this.mid_right_top, this.mid_right_mid, this.mid_right_bot, this.bot_right_corner];
-        for (let el in right) {
-            right[el].style.width = this.padding+"mm";
+        let top_bot = [this.top_left_corner, this.top_left, this.top_centre, this.top_right, this.top_right_corner, this.bot_left_corner, this.bot_left, this.bot_centre, this.bot_right, this.bot_right_corner];
+        for (let el in top_bot) {
+            top_bot[el].style.height = this.padding + "mm";
+            top_bot[el].style.outline = (this.pw) ? "1px dotted black" : "";
         }
-        let top = [this.top_left_corner, this.top_left, this.top_centre, this.top_right, this.top_right_corner];
-        for (let el in top) {
-            top[el].style.height = this.padding+"mm";
-        }
-        let bot = [this.bot_left_corner, this.bot_left, this.bot_centre, this.bot_right, this.bot_right_corner];
-        for (let el in bot) {
-            bot[el].style.height = this.padding+"mm";
-        }
-        this.el.style.width = this.width+"mm";
-        this.el.style.height = this.height+"mm";
+        // this.mid_centre.style.width = (this.width - (2 * this.padding)) + "mm";
+        // this.mid_centre.style.height = (this.height - (2 * this.padding)) + "mm";
+        let content_width = (this.width - (2 * this.padding)) + "mm"; // TODO count the cut markers
+        let content_height = (this.height - (2 * this.padding)) + "mm"; // TODO count the cut markers
+        // this.mid_centre.style.width = content_width;
+        // this.mid_centre.style.height = content_height;
+        this.el.style.width = this.width + "mm";
+        this.el.style.height = this.height + "mm";
+        this.table.style.width = this.width + "mm";
+        this.table.style.height = this.height + "mm";
+
+        this.grid.width = content_width;
+        this.grid.height = content_height;
+        this.grid.rows = this.rows;
+        this.grid.cols = this.cols;
+
+        this.grid.refresh();
     }
 }
+
+//TODO front page subclass
+//TODO back page subclass
 
 const PAPER_SIZES = {
     "A0": {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import './style.scss';
 import {Page, PAPER_SIZES, Ticket} from './classes.js';
 
-// use double hash
-// one known to every staff memebers
-// one known only by the brain to verify the correctness
-// does it make sense?
-
 const jsPDF = require('jspdf');
 const html2canvas = require('html2canvas');
 const sha256 = require('sha256');
@@ -13,20 +8,17 @@ const sha256 = require('sha256');
 const out_div = document.getElementById('out');
 
 const DEFAULT_WIDTH = 85.60;
-const DEFAULT_HEIGHT = 53.90;
+const DEFAULT_HEIGHT = 54.0;
 const DEFAULT_BLEED = 3.2;
-
-// STANDARD BLEED AREA 3.175
-
 
 new Settings();
 
 function Settings() {
     const PreviewTicket = new Ticket();
     const FrontPage = new Page();
-    const _static_text = document.getElementById('static_string'); //oninput
-    const _salt = document.getElementById('salt'); //oninput
-    const _quantity = document.getElementById('quantity'); //oninput
+    const _static_text = document.getElementById('static_string');
+    const _salt = document.getElementById('salt');
+    const _quantity = document.getElementById('quantity');
     const _ticket_width = document.getElementById('ticket_width');
     const _ticket_height = document.getElementById('ticket_height');
     const _front_graphic = document.getElementById('front-graphic');
@@ -34,12 +26,13 @@ function Settings() {
     const _front_bg_color = document.getElementById('front_bg_color');
     const _back_bg_color = document.getElementById('back_bg_color');
     const _bleed = document.getElementById('bleed');
-    const _qr_size = document.getElementById('qr-size');
+    const _qr_size = document.getElementById('qr_size');
     const _page_size = document.getElementById('page_size');
     const _custom_page_width = document.getElementById('page_width');
     const _custom_page_height = document.getElementById('page_height');
     const _page_orientation = document.getElementById('page_orientation');
     const _page_padding = document.getElementById('page_padding');
+    const _preview_toggle = document.getElementById('preview_toggle');
 
     let static_text = "";
     let salt = "";
@@ -53,45 +46,69 @@ function Settings() {
     let bleed = DEFAULT_BLEED;
     let qr_size = 40;
     let page_size = "";
-    let custom_width = "";
-    let custom_height = "";
-    let page_orientation = 0;
+    let page_width = 200;
+    let page_height = 100;
+    let page_orientation = 1;
     let page_padding = 10;
+    let preview = true;
+    let rows = 0;
+    let cols = 0;
 
     //init with default values
-    _static_text.value = static_text;
-    _salt.value = salt;
-    _quantity.value = quantity;
-    _ticket_width.value = ticket_width;
-    _ticket_height.value = ticket_height;
-    _front_graphic.value = front_graphic;
-    // back graphic?
-    _front_bg_color.value = front_bg_color;
-    _back_bg_color.value = back_bg_color;
-    _bleed.value = bleed;
-    _qr_size.value = qr_size;
-    for (let type in PAPER_SIZES) {
-        let option = document.createElement('option');
-        option.innerHTML = type + " " + PAPER_SIZES[type]["w"] + "x" + PAPER_SIZES[type]["h"];
-        option.value = type;
-        _page_size.appendChild(option);
-    }
-    _custom_page_width.value = 0;
-    _custom_page_height.value = 0;
-    _page_orientation.selectedIndex = page_orientation;
-    _page_padding.value = page_padding;
+    let init = () => {
+        _static_text.value = static_text;
+        _salt.value = salt;
+        _quantity.value = quantity;
+        _ticket_width.value = ticket_width;
+        _ticket_height.value = ticket_height;
+        _front_graphic.value = front_graphic;
+        // back graphic?
+        _front_bg_color.value = front_bg_color;
+        _back_bg_color.value = back_bg_color;
+        _bleed.value = bleed;
+        _qr_size.value = qr_size;
+        for (let type in PAPER_SIZES) {
+            let option = document.createElement('option');
+            option.innerHTML = type + " " + PAPER_SIZES[type]["w"] + "x" + PAPER_SIZES[type]["h"];
+            option.value = type;
+            _page_size.appendChild(option);
+        }
+        _custom_page_width.value = page_width;
+        _custom_page_height.value = page_height;
+        _page_orientation.selectedIndex = page_orientation;
+        _page_padding.value = page_padding;
+        _preview_toggle.checked = preview;
+        _custom_page_width.min = ticket_width+page_padding;
+        _custom_page_height.min = ticket_height+page_padding;
 
-    document.getElementById('td-pw-front').appendChild(PreviewTicket.front.el);
-    document.getElementById('td-pw-back').appendChild(PreviewTicket.back.el);
-    document.getElementById('out').appendChild(FrontPage.el);
+        document.getElementById('td-pw-front').appendChild(PreviewTicket.front.el);
+        document.getElementById('td-pw-back').appendChild(PreviewTicket.back.el);
+        document.getElementById('out').appendChild(FrontPage.el);
+    };
 
-    this.load = () => {
+    this.refresh = () => {
+        rows = Math.floor((page_height - 2 * page_padding - 2 * FrontPage.grid.marker) / (ticket_height + bleed)); // TODO count the cut markers
+        cols = Math.floor((page_width - 2 * page_padding - 2 * FrontPage.grid.marker) / (ticket_width + bleed)); // TODO count the cut markers
+        let ticket_per_page = rows * cols;
+        let pages_needed = Math.ceil(quantity/ticket_per_page);
+        let reduntant_tickets = pages_needed*ticket_per_page - quantity;
 
-        FrontPage.width = custom_width;
-        FrontPage.height = custom_height;
+        console.log(rows,cols);
+        console.log("Ticket per pagina: "+ticket_per_page);
+        console.log("Pagine necessarie: "+pages_needed);
+        console.log("Ticket in più:"+reduntant_tickets);
+
+        FrontPage.width = page_width;
+        FrontPage.height = page_height;
         FrontPage.padding = page_padding;
+        FrontPage.pw = preview;
+        FrontPage.rows = rows;
+        FrontPage.cols = cols;
+        FrontPage.grid.ticket_width = ticket_width+2*bleed;
+        FrontPage.grid.ticket_height = ticket_height+2*bleed;
+        FrontPage.grid.bleed = bleed;
 
-        _qr_size.max = Math.min(ticket_width, ticket_height);
+        PreviewTicket.pw = preview;
         PreviewTicket.width = ticket_width;
         PreviewTicket.height = ticket_height;
         PreviewTicket.front.addIMG(front_graphic);
@@ -106,106 +123,132 @@ function Settings() {
     };
 
     // events
-    _static_text.oninput = () => {
-        static_text = _static_text.value;
-        this.load()
-    };
-    _salt.oninput = () => {
-        salt = _salt.value;
-        this.load()
-    };
-    _quantity.oninput = () => {
-        quantity = _quantity.value;
-        this.load()
-    };
-    _ticket_width.oninput = () => {
-        ticket_width = _ticket_width.value;
-        this.load()
-    };
-    _ticket_height.oninput = () => {
-        ticket_height = _ticket_height.value;
-        this.load()
-    };
-    _front_graphic.onchange = (evt) => {
-        let tgt = evt.target || window.event.srcElement,
-            files = tgt.files;
-        // FileReader support
-        if (FileReader && files && files.length) {
-            let fr = new FileReader();
-            fr.onload = () => {
-                front_graphic = fr.result;
-                this.load();
-            };
-            fr.readAsDataURL(files[0]);
-        }
-    };
-    _front_bg_color.onchange = () => {
-        front_bg_color = _front_bg_color.value;
-        this.load()
-    };
-    _back_bg_color.onchange = () => {
-        back_bg_color = _back_bg_color.value;
-        this.load()
-    };
-    _bleed.oninput = () => {
-        bleed = _bleed.value;
-        this.load()
-    };
-    _qr_size.oninput = () => {
-        qr_size = _qr_size.value;
-        this.load()
-    };
-    _page_size.onchange = () => {
-        if (_page_size.value === "") {
-            _custom_page_width.value = 0;
-            _custom_page_height.value = 0;
-            page_size = "";
-            return;
-        }
+    let events = () => {
+        _static_text.oninput = () => {
+            static_text = _static_text.value;
+            this.refresh()
+        };
+        _salt.oninput = () => {
+            salt = _salt.value;
+            this.refresh()
+        };
+        _quantity.oninput = () => {
+            quantity = parseInt(_quantity.value);
+            this.refresh()
+        };
+        _ticket_width.oninput = () => {
+            ticket_width = parseFloat(_ticket_width.value);
+            _qr_size.max = Math.min(ticket_width, ticket_height);
+            _custom_page_width.min = ticket_width+page_padding;
+            this.refresh()
+        };
+        _ticket_height.oninput = () => {
+            ticket_height = parseFloat(_ticket_height.value);
+            _qr_size.max = Math.min(ticket_width, ticket_height);
+            _custom_page_height.min = ticket_height+page_padding;
+            this.refresh()
+        };
+        _front_graphic.onchange = (evt) => {
+            let tgt = evt.target || window.event.srcElement,
+                files = tgt.files;
+            // FileReader support
+            if (FileReader && files && files.length) {
+                let fr = new FileReader();
+                fr.onload = () => {
+                    front_graphic = fr.result;
+                    this.refresh();
+                };
+                fr.readAsDataURL(files[0]);
+            }
+        };
+        _front_bg_color.onchange = () => {
+            front_bg_color = _front_bg_color.value;
+            this.refresh()
+        };
+        _back_bg_color.onchange = () => {
+            back_bg_color = _back_bg_color.value;
+            this.refresh()
+        };
+        _bleed.oninput = () => {
+            bleed = parseFloat(_bleed.value);
+            this.refresh()
+        };
+        _qr_size.oninput = () => {
+            qr_size = parseFloat(_qr_size.value);
+            this.refresh()
+        };
+        _page_size.onchange = () => {
+            if (_page_size.value === "") {
+                _custom_page_width.value = 0;
+                _custom_page_height.value = 0;
+                page_size = "";
+                return;
+            }
 
-        page_size = _page_size.value;
-        let d = PAPER_SIZES[page_size];
-        if (_page_orientation.selectedIndex === 0) {
-            custom_width = d.w;
-            custom_height = d.h;
-        } else {
-            custom_width = d.h;
-            custom_height = d.w;
-        }
-        _custom_page_width.value = custom_width;
-        _custom_page_height.value = custom_height;
-        console.log(d.w, d.h);
-        this.load();
-    };
-    _custom_page_width.oninput = () => {
-        custom_width = _custom_page_width.value;
-        _page_size.selectedIndex = 0;
-        this.load();
-    };
-    _custom_page_height.oninput = () => {
-        custom_height = _custom_page_height.value;
-        _page_size.selectedIndex = 0;
-        this.load();
-    };
-    _page_orientation.onchange = () => {
-        if (_page_orientation.selectedIndex === 0) {
-            custom_width = PAPER_SIZES[page_size]["w"];
-            custom_height = PAPER_SIZES[page_size]["h"];
-        } else {
-            custom_width = PAPER_SIZES[page_size]["h"];
-            custom_height = PAPER_SIZES[page_size]["w"];
-        }
+            page_size = _page_size.value;
+            let d = PAPER_SIZES[page_size];
+            if (_page_orientation.selectedIndex === 0) {
+                page_width = d.w;
+                page_height = d.h;
+            } else {
+                page_width = d.h;
+                page_height = d.w;
+            }
+            _custom_page_width.value = page_width;
+            _custom_page_height.value = page_height;
+            this.refresh();
+        };
+        _custom_page_width.oninput = () => {
+            page_width = parseFloat(_custom_page_width.value);
+            _page_size.selectedIndex = 0;
+            this.refresh();
+        };
+        _custom_page_height.oninput = () => {
+            page_height = parseFloat(_custom_page_height.value);
+            _page_size.selectedIndex = 0;
+            this.refresh();
+        };
+        _page_orientation.onchange = () => {
+            if (_page_orientation.selectedIndex === 0) {
+                if (page_size === "") {
+                    let new_width = Math.min(page_width, page_height);
+                    let new_height = Math.max(page_width, page_height);
+                    page_width = new_width;
+                    page_height = new_height;
+                } else {
+                    page_width = PAPER_SIZES[page_size]["w"];
+                    page_height = PAPER_SIZES[page_size]["h"];
+                }
+            } else {
+                if (page_size === "") {
+                    let new_width = Math.max(page_width, page_height);
+                    let new_height = Math.min(page_width, page_height);
+                    page_width = new_width;
+                    page_height = new_height;
+                } else {
+                    page_width = PAPER_SIZES[page_size]["h"];
+                    page_height = PAPER_SIZES[page_size]["w"];
+                }
+            }
 
-        _custom_page_width.value = custom_width;
-        _custom_page_height.value = custom_height;
-        this.load();
-    };
-    _page_padding.oninput = () => {
-        page_padding = _page_padding.value;
-        this.load()
+            _custom_page_width.value = page_width;
+            _custom_page_height.value = page_height;
+            this.refresh();
+        };
+        _page_padding.oninput = () => {
+            page_padding = parseInt(_page_padding.value);
+            this.refresh()
+        };
+        _preview_toggle.onchange = () => {
+            preview = _preview_toggle.checked;
+            this.refresh();
+        };
     };
 
-    this.load();
+
+    init();
+    events();
+    this.refresh();
 }
 
 function gen_hashes(static_string, salt, quantity) {
